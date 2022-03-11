5d3e1b9184e47edc8bf23fe50d4dcfacaf2be246
Yunzhu Jin <jyz022020@gmail.com>
2022-03-10 23:24:54 -0800

    test in page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
     var content = $("#search-content").val();
     alert(content);
     // var response = fetch("http://api.openweathermap.org/geo/1.0/direct?q="+content+"&limit=2&appid=a8adc8105c06ae9463caae0bc562930c");
-    var response2 = fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=");
+    var response2 = fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=a8adc8105c06ae9463caae0bc562930c");
     response2.then(response => {
         console.log(response);
       });
